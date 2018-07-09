1551e9687d62d3035a2c84d3350e8126ebca79a3
BoxingP <boxingp@gmail.com>
2018-07-10 00:40:14 +0800

    L-4: modify ex3.py as study drills asked. Boxing

--- ex3.py
+++ ex3.py
@@ -1,14 +1,17 @@
 print "I will now count my chickens:"
 
+# + is for addition, / is for division, - is for subtraction, * is for multiplication, % is for remainder.
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
 print "Now I will count eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Integer division returns the result of division rounded towards zero.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# comparison operators
 print 3 + 2 < 5 - 7
 
 print "What is 3 + 2?", 3 + 2
