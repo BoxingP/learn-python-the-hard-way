acdf3541b3bccee688e5875258bfd977d889e59f
BoxingP <boxingp@gmail.com>
2018-07-11 12:45:28 +0800

    L-9: modify ex5.py as study drills asked. Boxing

--- ex5.py
+++ ex5.py
@@ -1,18 +1,20 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+height_cm = height / 0.393701
+weight = 180 # lbs
+weight_kg = weight / 2.20462
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches (%.2f cm) tall." % (height, height_cm)
+print "He's %d pounds (%.2f kg) heavy." % (weight, weight_kg)
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-        my_age, my_height, my_weight, my_age + my_height + my_weight)
+        age, height, weight, age + height + weight)
