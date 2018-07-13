0bf5aec136312c5b37faf9bfe237e8de77fe6b6b
BoxingP <boxingp@gmail.com>
2018-07-13 12:26:02 +0800

    L-11: modify ex6.py as study drills asked. Boxing

--- ex6.py
+++ ex6.py
@@ -1,20 +1,30 @@
+# set a string with an integer variable
 x = "There are %d types of people." % 10
+# set two strings
 binary = "binary"
 do_not = "don't"
+# set a string with two string variables
 y = "Those who know %s and those who %s." % (binary, do_not)
 
+# print the two strings out
 print x
 print y
 
+# print two strings with string set before
 print "I said: %r." % x
 print "I also said: '%s'." % y
 
+# set a boolean value
 hilarious = False
+# set a string with variable
 joke_evaluation = "Isn't that joke so funny?! %r"
 
+# print the string out with a variable convert from boolean to string
 print joke_evaluation % hilarious
 
+# set two strings
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# append one string to another and print the result out
 print w + e
