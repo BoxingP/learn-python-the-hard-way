651ec8497f1a1aad23663c02e961e239b238c532
BoxingP <boxingp@gmail.com>
2018-07-14 15:10:34 +0800

    L-18: add comment in ex9.py. Boxing

--- ex9.py
+++ ex9.py
@@ -1,6 +1,7 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# use \n to specify a new line
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days: ", days
