ace20797a14cdb0f516b14c326a6b48511edd6c1
BoxingP <boxingp@gmail.com>
2018-07-21 15:12:05 +0800

    L-28: add comments in ex15.py. Boxing

--- ex15.py
+++ ex15.py
@@ -1,15 +1,22 @@
+# import argv
 from sys import argv
 
+# unpack arguments
 script, filename = argv
 
+# open file
 txt = open(filename)
 
+# print file name
 print "Here's your file %r:" % filename
+# read file content and print it out
 print txt.read()
 
+# input file name
 print "Type the filename again:"
 file_again = raw_input("> ")
 
+# open file with the name inputed
 txt_again = open(file_again)
 
 print txt_again.read()
