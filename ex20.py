09db48d8a58854a866a934c1f11441c5fff01f77
BoxingP <boxingp@gmail.com>
2018-07-24 17:13:10 +0800

    L-38: modify ex20.py and create a script as study drills asked. Boxing

--- ex20.py
+++ ex20.py
@@ -2,12 +2,15 @@ from sys import argv
 
 script, input_file = argv
 
+# define a function to print out all file content
 def print_all(f):
     print f.read()
 
+# change the file object's position to the beginning of the file
 def rewind(f):
     f.seek(0)
 
+# print out one line from the file and the line count
 def print_a_line(line_count, f):
     print line_count, f.readline()
 
@@ -23,9 +26,11 @@ rewind(current_file)
 
 print "Let's print three lines:"
 
+# define current line amount as 1
 current_line = 1
 print_a_line(current_line, current_file)
 
+# increase the line amount
 current_line = current_line + 1
 print_a_line(current_line, current_file)
 
