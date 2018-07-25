6ea95f2f37a7d3621c7678f7ae8f0b2108d24b21
BoxingP <boxingp@gmail.com>
2018-07-25 12:10:35 +0800

    L-40: modify ex21.py as study drills asked. Boxing

--- ex21.py
+++ ex21.py
@@ -27,6 +27,11 @@ print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 # A puzzle for the extra credit, type it in anyway
 print "Here is a puzzle."
 
+def formula(a, b, c, d):
+    return a + b - c * d / 2
+
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+value = formula(age, height, weight, iq)
 
 print "That becomes: ", what, "Can you do it by hand?"
+print "This value ", value, "is what function returns."
