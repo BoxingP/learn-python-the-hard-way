77fc326cb1a8af317ab0954803279faf440f18f9
BoxingP <boxingp@gmail.com>
2019-10-19 18:18:52 +0800

    L-51: modify ex30.py as study drills asked. Boxing

--- ex30.py
+++ ex30.py
@@ -1,23 +1,33 @@
+# define people value
 people = 30
+# define cars value
 cars = 40
+# define buses value
 buses = 15
 
-
+# if cars is bigger than people, will print "take the cars"
 if cars > people:
     print "We should take the cars."
+# if cars is less than people, will print "not take the cars"
 elif cars < people:
     print "We should not take the cars."
+# otherwise will print "can't decide"
 else:
     print "We can't decide."
 
+# if buses is bigger than cars, will print "too many buses"
 if buses > cars:
     print "That's too many buses."
+# if buses is less than cars, will print "take the buses"
 elif buses < cars:
     print "Maybe we could take the buses."
+# otherwise will print "can't decide"
 else:
     print "We still can't decide."
 
+# if people is bigger than buses, will print "take the buses"
 if people > buses:
     print "Alright, let's just take the buses."
+# otherwise will print "stay home"
 else:
     print "Fine, let's stay home then."
