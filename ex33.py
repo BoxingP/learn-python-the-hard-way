585c21805ec457cbe7be258d90cbd0ddcf238da3
BoxingP <boxingp@gmail.com>
2019-10-23 00:27:49 +0800

    L-57: rewrite ex33.py by converting while-loop to a function with limit as a variable. Boxing

--- ex33.py
+++ ex33.py
@@ -1,14 +1,18 @@
-i = 0
-numbers = []
+def add_number_to_list(limit):
+    i = 0
+    numbers = []
+    while i < limit:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+    return numbers
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
-
+print "Input how many numbers do you want to add"
+limit = raw_input("> ")
+numbers = add_number_to_list(int(limit))
 
 print "The numbers: "
 
