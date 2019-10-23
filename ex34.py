0a47f4190d65373377f10371786490a02fafbdc9
BoxingP <boxingp@gmail.com>
2019-10-23 20:26:53 +0800

    L-60: exercise 34. Boxing

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The second animal is at %d and is a %s" % (1, animals[1])
+print "The third animal is at %d and is a %s" % (2, animals[2])
+print "The first animal is at %d and is a %s" % (0, animals[0])
+print "The fourth animal is at %d and is a %s" % (3, animals[3])
+print "The fifth animal is at %d and is a %s" % (4, animals[4])
+print "The third animal is at %d and is a %s" % (2, animals[2])
+print "The sixth animal is at %d and is a %s" % (5, animals[5])
+print "The fifth animal is at %d and is a %s" % (4, animals[4])
