851275aadf31f90d641061acab86bba714eafa57
BoxingP <boxingp@gmail.com>
2019-10-23 13:32:11 +0800

    L-58: rewrite ex33.py by adding a variable to define how much numbers increment by. Boxing

--- ex33.py
+++ ex33.py
@@ -1,18 +1,21 @@
-def add_number_to_list(limit):
+def add_number_to_list(limit, increment):
     i = 0
     numbers = []
     while i < limit:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + increment
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
     return numbers
 
-print "Input how many numbers do you want to add"
+print "Input the numbers limit which numbers are less than it"
 limit = raw_input("> ")
-numbers = add_number_to_list(int(limit))
+print "Input the increment between numbers"
+increment = raw_input("> ")
+
+numbers = add_number_to_list(int(limit), int(increment))
 
 print "The numbers: "
 
