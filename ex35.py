54cf321376ed4b82de432525ab1f82afbe1d3737
BoxingP <boxingp@gmail.com>
2019-10-26 02:53:00 +0800

    L-63: use int() to check whether the input is a number. Boxing

--- ex35.py
+++ ex35.py
@@ -4,17 +4,15 @@ def gold_room():
     print "This room is full of gold. How much do you take?"
 
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    try:
         how_much = int(next)
-    else:
+        if how_much < 50:
+            print "Nice, you're not greedy, you win!"
+            exit(0)
+        else:
+            dead("You greedy bastard!")
+    except ValueError:
         dead("Man, learn to type a number.")
-
-    if how_much < 50:
-        print "Nice, you're not greedy, you win!"
-        exit(0)
-    else:
-        dead("You greedy bastard!")
-
 
 def bear_room():
     print "There is a bear here."
