9215c17b8ee10bdeb22c49f15a782f61883bfc95
BoxingP <boxingp@gmail.com>
2019-11-30 21:40:14 +0800

    L-73: add comments for ex42.py. Boxing

--- ex42.py
+++ ex42.py
@@ -2,48 +2,50 @@
 class Animal(object):
     pass
 
-## ??
+## Dog is-a Animal
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## name is-a instance variable
         self.name = name
 
-## ??
+## Cat is-a Animal
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## name is-a instance variable
         self.name = name
 
-## ??
+## Person is-a object
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## name is-a instance variable
         self.name = name
 
         ## Person has-a pet of some kind
         self.pet = None
 
-## ??
+## Employee is-a Person
 class Employee(Person):
     
     def __init__(self, name, salary):
-        ## ?? hmm what is this strange magic?
+        ## use super() to call the __init__() of the Person class
+        ## the first parameter is the subclass, and the second is 
+        ## an object that is an instance of the subclass
         super(Employee, self).__init__(name)
-        ## ??
+        ## salary is-a instance variable
         self.salary = salary
 
-## ??
+## Fish is-a object
 class Fish(object):
     pass
 
-## ??
+## Salmon is-a Fish
 class Salmon(Fish):
     pass
 
-## ??
+## Halibut is-a Fish
 class Halibut(Fish):
     pass
 
@@ -51,26 +53,26 @@ class Halibut(Fish):
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## saton is-a Cat
 satan = Cat("Satan")
 
-## ??
+## mary is-a Person
 mary = Person("Mary")
 
-## ??
+## mary has-a pet which is satan
 mary.pet = satan
 
-## ??
+## frank is-a Employee
 frank = Employee("Frank", 120000)
 
-## ??
+## frank has-a pet which is rover
 frank.pet = rover
 
-## ??
+## flipper is-a Fish
 flipper = Fish()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon()
 
-## ??
+## harry is-a Halibut
 harry = Halibut()
